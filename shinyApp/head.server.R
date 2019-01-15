6917714e8cb86e5a7e017adda7d4490432f8e272
Yuhui Yao <bolus123@users.noreply.github.com>
2019-01-15 17:44:06 -0600

    update

--- shinyApp/head.server.R
+++ shinyApp/head.server.R
@@ -15,8 +15,7 @@ check.packages(packages)
 
 Statistics <- function(Data){
 
-    m <- dim(Data)[1]
-    n <- dim(Data)[2]
+    n <- length(Data)
     
     x <- as.vector(Data)
     
@@ -27,9 +26,9 @@ Statistics <- function(Data){
     ks.norm <- ks.test(x, "pnorm")$p.value
     
     out <- list(
-        Metric = c('m', 'n', 'Skewness', 'Kurtosis', 'Max', 'Min', 'Q-0.01', 'Q-0.05', 'Q-0.1', 'Q-0.2', 'Q-0.25', 'Q-0.5'
+        Metric = c('n', 'Skewness', 'Kurtosis', 'Max', 'Min', 'Q-0.01', 'Q-0.05', 'Q-0.1', 'Q-0.2', 'Q-0.25', 'Q-0.5'
             , 'Q-0.75', 'Q-0.8', 'Q-0.9', 'Q-0.95', 'Q-0.99', 'KS P-value'),
-        Value = c(m, n, skew, kurt, quantiles, ks.norm)
+        Value = c(n, skew, kurt, quantiles, ks.norm)
     )
     
     return(out)
